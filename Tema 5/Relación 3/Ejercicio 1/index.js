b47987bfcc7462d793c16c4d5419de9e75af99d8
Samuel Gómez Pérez <samgope100@gmail.com>
2022-11-13 19:26:30 +0100

    Continuación relación 3 Tema 5

--- Tema 5/Relación 3/Ejercicio 1/index.js
+++ Tema 5/Relación 3/Ejercicio 1/index.js
@@ -1,12 +1,51 @@
-window.onload() = function() {
-    horas = document.getElementById("horas");
-    minutos = document.getElementById("minutos");
-    segundos = document.getElementById("segundos");
+window.onload  = () => {
+    horas_container = document.getElementById("horas");
+    minutos_container = document.getElementById("minutos");
+    segundos_container = document.getElementById("segundos");
     start = document.getElementById("start");
     stopp = document.getElementById("stop");
     reset = document.getElementById("reset");
 
-    start.addEventListener("click");
-    reset.addEventListener("click");
-    stopp.addEventListener("click");
+    start.addEventListener("click", () => {
+        interval = setInterval(() => {
+            horas = parseInt(horas_container.innerHTML);
+            minutos = parseInt(minutos_container.innerHTML);
+            segundos = parseInt(segundos_container.innerHTML);
+            if(segundos < 59){
+                if(segundos < 9){
+                    segundos_container.innerHTML = "0" + (++segundos);
+                }
+                else segundos_container.innerHTML++;
+            }
+            else{
+                segundos_container.innerHTML = "00";
+                if(minutos < 59){
+                    if(minutos < 9){
+                        minutos_container.innerHTML = "0" + (++minutos);
+                    }
+                    else minutos_container.innerHTML++;
+                }
+                else{
+                    minutos_container.innerHTML = "00";
+                    if(horas < 9){
+                        horas_container.innerHTML = "0" + (++horas);
+                    }
+                    else horas_container.innerHTML++;
+                }
+            }
+        }, 1000);
+    });
+
+    stopp.addEventListener("click", () => {
+        window.clearInterval(interval);
+        interval = null;
+    });
+
+    reset.addEventListener("click", () => {
+        horas_container = minutos_container.innerHTML = segundos_container.innerHTML = "00";
+        if(interval){
+            window.clearInterval(interval);
+        }
+        interval = null;
+    });
 };
---
+++ Tema 5/Relación 3/Ejercicio 4/index.js
@@ -0,0 +1,59 @@
+window.onload = () => {
+    input = document.querySelector("input");
+    addBtn = document.getElementById("añadir");
+    ul = document.querySelector("ul");
+    empty = document.querySelector(".empty");
+
+    addBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    text = input.value;
+
+    if(text !== ""){
+        li = document.createElement("li");
+        li.textContent = text;
+
+        // li.appendChild(addModifyBtn());
+        li.appendChild(addDeleteBtn());
+        ul.appendChild(li);
+
+        input.value = "";
+        empty.style.display = "none";
+    }
+    });
+};
+
+// function addModifyBtn() {
+//   modifyBtn = document.createElement("button");
+
+//   modifyBtn.textContent = "Modify";
+//   modifyBtn.className = "btn-modify";
+
+//   modifyBtn.addEventListener("click", (e) => {
+//     item = e.target.parentElement;
+//     item.contentEditable = true;
+//   });
+
+//   return modifyBtn;
+// }
+
+function addDeleteBtn() {
+  deleteBtn = document.createElement("button");
+//   img = document.createElement("img");
+
+//   deleteBtn.appendChild(img);
+//   img.src="./imagenes/papelera.png";
+  deleteBtn.textContent = "X";
+
+  deleteBtn.addEventListener("click", (e) => {
+    item = e.target.parentElement;
+    ul.removeChild(item);
+
+    items = document.querySelectorAll("li");
+
+    if (items.length === 0) {
+      empty.style.display = "block";
+    }
+  });
+
+  return deleteBtn;
+}
