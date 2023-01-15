ef7a8572fed94a48ac16e484b33c576130348302
Samuel Gómez Pérez <samgope100@gmail.com>
2023-01-15 16:58:55 +0100

    continuación api películas Vue

--- Vue/Api peliculas Vue/index.js
+++ Vue/Api peliculas Vue/index.js
@@ -9,40 +9,42 @@ const {createApp} = Vue
 createApp({
     data(){
         return{
-            peliculas: [
-                // {
-                //     Title: "One Piece",
-                //     Year: "1999-",
-                //     Poster: "https://m.media-amazon.com/images/M/MV5BODcwNWE3OTMtMDc3MS00NDFjLWE1OTAtNDU3NjgxODMxY2UyXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
-                // },
-                // {
-                //     Title: "One Flew Over the Cuckoo's Nest",
-                //     Year: "1975",
-                //     Poster: "https://m.media-amazon.com/images/M/MV5BZjA0OWVhOTAtYWQxNi00YzNhLWI4ZjYtNjFjZTEyYjJlNDVlL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
-                // },
-                // {
-                //     Title: "Air Force One",
-                //     Year: "1997",
-                //     Poster: "https://m.media-amazon.com/images/M/MV5BZmNiNTQ5ZjMtOThjYS00NDc2LWFlMWMtZTM5OTA1NjRmMGM4XkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_SX300.jpg"
-                // },
-            ],
+            peliculas: [],
+            informacionAmpliada: [],
             busqueda: "",
             informacion: false,
-            desinformacion: true
+            pagina: 1
         }
     },
     methods: {
         lanzarPeticion(){
             axios
-                .get('http://www.omdbapi.com/?apikey=f14b0120&s=one')
-                .then(response => (this.info = response))
-                this.peliculas.push(response);
-        }
+                .get('http://www.omdbapi.com/?apikey=f14b0120&s=' + this.busqueda)
+                .then(response => {
+                for(i=0; i<response.data.Search.length; i++) 
+                    this.peliculas.push(response.data.Search[i])
+                    this.informacion = true
+                });
+        },
+        borrar(){
+            this.peliculas = [],
+            this.informacionAmpliada = [],
+            this.pagina = 1,
+            this.informacion = false;
+            this.busqueda = "";
+        },
+        // ampliarInformacionPeli(pelicula){
+        //     axios.get("https://www.omdbapi.com/?i=" + pelicula.imdbID + "&apikey=f14b0120")
+        //     .then(responseDataPlus => {this.informacionAmpliada = responseDataPlus.data;
+        //         this.divInfo = true;
+        //         this.transparencia = true;
+        //         this.disableScroll();
+        //     });
+        // },
     },
     computed: {
 
     },
     mounted(){
-
     }
 }).mount('#vue')
